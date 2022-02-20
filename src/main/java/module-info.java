01d55a242a98707fc3c918d0fd30ebecb7183975
mehdiben7 <mehdi.benouhoud7@gmail.com>
2022-02-20 01:25:57 -0500

    Update SDK to OpenJFX, Chanfang's lib to Maven package     changed project name to "TonyHawkSimulator"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ca.remechda.remechda {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens ca.thetonyhawks.tonyhawksimulator to javafx.controls;
     exports ca.thetonyhawks.tonyhawksimulator;
