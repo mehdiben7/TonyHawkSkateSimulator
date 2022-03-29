aa8ee8b51a18410f42a61ea7ec36a9b3af0504db
Changfan <a2309414184@gmail.com>
2022-03-29 10:13:51 -0400

    database

--- src/main/java/ca/thetonyhawks/tonyhawksimulator/ImportDatabaseController.java
+++ src/main/java/ca/thetonyhawks/tonyhawksimulator/ImportDatabaseController.java
@@ -1,8 +1,73 @@
 package ca.thetonyhawks.tonyhawksimulator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
+import java.sql.*;
 
 /**
  *  The Controller class linked to the import database user interface window of the simulator <br>
  *  whose FXML file is <em>ImportDatabase.fxml</em>
  */
-public class ImportDatabaseController {
+public class ImportDatabaseController
+{
+
+    @FXML
+    private TableColumn name,acceleration;
+
+    private Stage importDatabaseStage;
+
+    String engine="jdbc:sqlite";
+
+
+    public Connection getConnection(String db)
+    {   String connectingString;
+//        String connectingString=engine+":"+db;
+        try
+        {
+
+            connectingString="jdbc:sqlite:C:/Users/2067309/Documents/TonyHawkSimulator.db";
+            Connection dbConnection=DriverManager.getConnection(connectingString);
+            return dbConnection;
+        }catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    public void ImportEventHandler(ActionEvent actionEvent)
+    {
+
+    }
+
+
+
+    public void selectFileEventHandler(ActionEvent actionEvent)
+    {
+        FileChooser chooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Database Files", "*.db");
+        chooser.getExtensionFilters().add(extFilter);
+
+        File file = chooser.showOpenDialog(importDatabaseStage);
+        if (file != null)
+        {
+            Connection connection = getConnection(file.getPath());
+            String query="SELECT * From TonyHawkSimulator";
+            try
+            {
+                Statement statement=connection.createStatement();
+                ResultSet   resultSet=statement.executeQuery(query);
+                name.getColumns().add(resultSet.getString("Name"));
+
+
+            }catch(SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+
+        }
+    }
 }
