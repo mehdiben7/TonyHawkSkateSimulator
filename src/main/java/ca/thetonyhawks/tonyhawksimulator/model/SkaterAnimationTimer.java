6172ac81b1a5e4259145480cdaa4bec5f0edac70
mehdiben7 <mehdi.benouhoud7@gmail.com>
2022-04-11 15:19:15 -0400

    Energy graph solved commit

--- src/main/java/ca/thetonyhawks/tonyhawksimulator/model/SkaterAnimationTimer.java
+++ src/main/java/ca/thetonyhawks/tonyhawksimulator/model/SkaterAnimationTimer.java
@@ -1,6 +1,7 @@
 package ca.thetonyhawks.tonyhawksimulator.model;
 
 import ca.thetonyhawks.tonyhawksimulator.controller.TonyHawkSimulatorController;
+import ca.thetonyhawks.tonyhawksimulator.model.planes.AngledSkaterPlane;
 import javafx.animation.AnimationTimer;
 
 /**
@@ -40,6 +41,12 @@ public class SkaterAnimationTimer extends AnimationTimer {
 
         animationModel.getController().updateEnergyValues(animationModel.getSkater().kineticEnergyProperty().get(),
                                             animationModel.getSkater().potentialGravitationalEnergyProperty().get());
+
+
+        if(animationModel.getPlane() instanceof AngledSkaterPlane && animationModel.getSkater().positionProperty().get() > AngledSkaterPlane.PLANE_LENGTH) {
+            this.pause();
+            animationModel.getController().pauseAnimation();
+        }
 
     }
 
